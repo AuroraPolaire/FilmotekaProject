7edb0d90bf4d7cfa8e2d3026c2adcba47ff56562
Pavlo Kliuchenko <88260408+Kliuchenko@users.noreply.github.com>
2023-02-11 21:45:27 +0200

    start

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,1 +1,8 @@
-//
+          function changeTheme(isChecked) {
+            if (isChecked) {
+              document.body.setAttribute('dark', '');
+            } else {
+              document.body.removeAttribute('dark');
+            }
+          }
+       
