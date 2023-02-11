dd8cb9f409e97d776e03f9fbaaed797941b3bf41
AliceChpmnk <alisablog50@gmail.com>
2023-02-11 21:50:00 +0200

    trailer/teaser find

---
+++ src/js/findTrailer.js
@@ -0,0 +1,18 @@
+export function findTrailer(videos) {
+    if (videos.length == 0) {
+        return;
+    }
+
+    for (const v of videos) {
+        switch (v.type) {
+            case "Trailer":
+                return v.key;
+                break;
+            case "Teaser":
+                return v.key;
+                break;
+            default:
+            return videos[0].key;
+        }
+    }
+}
