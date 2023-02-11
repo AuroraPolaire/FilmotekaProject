b0e9c1304f27569891d09e7fd16db02ee5d14741
Kliuchenko <pasha.kluchenko@gmail.com>
2023-02-11 22:57:26 +0200

    complete transitions

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,8 +1,34 @@
-          function changeTheme(isChecked) {
-            if (isChecked) {
-              document.body.setAttribute('dark', '');
-            } else {
-              document.body.removeAttribute('dark');
-            }
-          }
-       
+// const themeSwitchButton = document.getElementById("theme-switch-button");
+// const body = document.body;
+// const text = document.getElementById("text");
+
+// themeSwitchButton.addEventListener("click", function () {
+//   if (body.classList.contains("dark-theme")) {
+//     body.classList.remove("dark-theme");
+//   } else {
+//     body.classList.add("dark-theme");
+//   }
+// });
+
+// const themeSwitchButton = document.getElementById("theme-switch-button");
+// const body = document.body;
+// const themeIcon = themeSwitchButton.querySelector(".theme-icon");
+
+// themeSwitchButton.addEventListener("click", function () {
+//   if (body.classList.contains("dark-theme")) {
+//     body.classList.remove("dark-theme");
+//     themeIcon.innerHTML = '<use href="./images/symbol-defs.svg#sun"></use>';
+//   } else {
+//     body.classList.add("dark-theme");
+//     themeIcon.innerHTML = '<use href="./images/symbol-defs.svg#moon"></use>';
+//   }
+// });
+
+const themeSwitchButton = document.getElementById("theme-switch-button");
+const body = document.body;
+const iconEl = document.querySelector('.theme-icon')
+
+themeSwitchButton.addEventListener("click", function () {
+    body.classList.toggle("dark-theme");
+    iconEl.classList.toggle("dark-theme");
+});
