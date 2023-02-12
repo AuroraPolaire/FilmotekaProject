be8a2d434f9b2d987cd4e1544631a900d62d31f8
Vitalii Pavliuk <pavlyuk.vitaliy@gmail.com>
2023-02-12 11:15:45 +0200

    Update buttons.js

--- src/js/buttons.js
+++ src/js/buttons.js
@@ -1,3 +1,24 @@
+import { showLoginForm } from './auth-ui';
+
+import { initializeApp } from 'firebase/app';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { getFirestore, setDoc, doc } from 'firebase/firestore';
+
+const firebaseApp = initializeApp({
+  apiKey: 'AIzaSyAQNEF1EZQq4hRQq21AEqmnTA4ysock-bY',
+  authDomain: 'filmoteka-77997.firebaseapp.com',
+  projectId: 'filmoteka-77997',
+  storageBucket: 'filmoteka-77997.appspot.com',
+  messagingSenderId: '555325927364',
+  appId: '1:555325927364:web:5f447ca8d4265db75c3e91',
+  measurementId: 'G-LZ05WYL8RJ',
+});
+
+// <<<<<<<<<< FIREBASE AUTHENTICATION >>>>>>>>>>
+
+const auth = getAuth(firebaseApp);
+const firestore = getFirestore(firebaseApp);
+
 // Add Film To WATCHED
 
 export const addFilmToWatched = async (filmID, filmTitle) => {
